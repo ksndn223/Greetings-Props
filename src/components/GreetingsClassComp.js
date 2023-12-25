ca5e5e27815ed06deb2e6400a709fcbfff06ab9f
ksndn223 <11366993+ksndn223@users.noreply.github.com>
2023-12-25 03:06:47 -0500

    Created GreetingsClassComp.js

---
+++ src/components/GreetingsClassComp.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class GreetingsClassComp extends React.Component {
+    render() { 
+        return ( <h3 align="justified"> Hello there Jane's team!</h3>);
+    }
+}
+ 
+export default GreetingsClassComp;
