89f6b80b04deba3a51731b2c38a64105eeb123ea
ksndn223 <11366993+ksndn223@users.noreply.github.com>
2023-12-25 03:05:29 -0500

    Created Greetings.js

---
+++ src/components/Greetings.js
@@ -0,0 +1,11 @@
+import '../App.css';
+
+const Greetings  = (props) => {
+    return (
+        <div>
+            <h3> Hello {props.name}</h3>
+        </div>
+      );
+}
+ 
+export default Greetings;
