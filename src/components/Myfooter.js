93287348cd033d4d1e979865d17009fb7913e7fe
ksndn223 <11366993+ksndn223@users.noreply.github.com>
2023-12-25 03:10:23 -0500

    Created Myfooter.js

---
+++ src/components/Myfooter.js
@@ -0,0 +1,9 @@
+const MyFooter = () => {
+    return (
+        
+        <div className="bg-success">
+                <h6 align="left"> App footer</h6>
+        </div>
+     );
+}
+export default MyFooter;
